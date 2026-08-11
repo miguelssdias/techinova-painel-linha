2fd9243cfb34fd86a425188d8cbea381295268ee
miguelssdias <miguelssdias2012@gmail.com>
2026-08-11 18:13:29 -0300

    Ajusta Formula de conversao localmente

--- js/painel.js
+++ js/painel.js
@@ -7,7 +7,7 @@ async function carregarSensores() {
 }
 
 function converterTemperatura(leitura) {
-  return leitura * 9 / 5 + 32;
+  return (leitura - 32) * (5 / 9);
 }
 
 function desenharLinha(sensor) {
